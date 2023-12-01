ce0cd5e18bf767a90febcad296d7be1cffeffddd
Tim Gunnarsson <tim.awg@outlook.com>
2023-12-01 02:35:46 +0100

    Initial Commit

---
+++ Weather App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by Tim Gunnarsson on 2023-12-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
